7c6928e9ed6898dc92ad58189aa5b417ee5df2d7
obrad97 <janiobrad@gmail.com>
2021-10-17 02:37:33 +0200

    almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content')
+const thanksModal = document.querySelector('.thanks-modal');
 const modalClose = document.getElementById('modal-close');
+const thanksModalClose = document.querySelector('.thanks-btn');
 const openModal = document.querySelector('.inner-top-btn');
 const openPlanModal = document.querySelectorAll('.select-reward');
 const radioPlanSelect = document.querySelectorAll('.radio-btn');
@@ -7,10 +10,39 @@ const headlinePlanSelect = document.querySelectorAll('.plan-name');
 const bookmark = document.querySelector('.bookmark');
 const inputFields = document.querySelectorAll('.input-field');
 const pledgeButton = document.querySelectorAll('.pledge-btn');
-var currentMoney = 89914;
-var currentBackers = 5007;
+const currentMoneyDisplay = document.getElementById('current-sum');
+const currentBackersDisplay = document.getElementById('backers');
+const trackerLine = document.getElementById('inner-line');
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuHamburger = document.getElementById('menu-hamburger');
+const menuClose = document.getElementById('menu-close');
+var currentMoney = 10000;
+var currentBackers = 256;
+currentMoneyDisplay.innerText = `$${currentMoney.toLocaleString('en-US')}`;
+currentBackersDisplay.innerText = currentBackers.toLocaleString('en-US');
+trackerLine.style.width = `${parseInt(currentMoneyDisplay.innerText.split('$')[1].split(',')[0])}%`;
 
-bookmark.addEventListener('click', ()=> {
+
+menuHamburger.addEventListener('click', (e)=> {
+    mobileMenu.classList.add('mobile-menu-active');
+    document.body.style.overflow = 'hidden';
+})
+
+menuClose.addEventListener('click', (e)=> {
+    mobileMenu.classList.remove('mobile-menu-active');
+    document.body.style.overflow = 'auto';
+})
+
+mobileMenu.addEventListener('click',(e)=> {
+    if (e.target != e.currentTarget){
+        return
+    }else {
+        mobileMenu.classList.remove('mobile-menu-active');
+        document.body.style.overflow = 'auto';
+    }
+})
+
+bookmark.addEventListener('click', (e)=> {
     const text = bookmark.querySelector('p');
     if (bookmark.classList.contains('bookmark-active')) {
         bookmark.classList.remove('bookmark-active')
@@ -19,6 +51,13 @@ bookmark.addEventListener('click', ()=> {
         bookmark.classList.add('bookmark-active')
         text.innerText = 'Bookmarked'
     }
+})
+
+thanksModalClose.addEventListener('click', (e)=>{
+    thanksModal.style.display = "none";
+    modalContent.style.display = "block";
+    document.body.style.overflow = 'auto'
+    modal.classList.remove('modal-active');
 })
 
 modalClose.addEventListener('click',(e)=> {
@@ -60,6 +99,18 @@ openPlanModal.forEach((button, index)=> {
     })
 })
 
+radioPlanSelect.forEach((plan, index) => {
+    plan.addEventListener('click', (e)=> {
+        openPlan(index)
+    })
+})
+
+headlinePlanSelect.forEach((plan, index) => {
+    plan.addEventListener('click', (e)=> {
+        openPlan(index)
+    })
+})
+
 const openPlan = (index) => {
     const planCard = document.getElementsByClassName('modal-donation-card');
     const errorMsg = document.querySelectorAll('.modal-donation-card-pledge');
@@ -77,18 +128,6 @@ const openPlan = (index) => {
     }
 }
 
-radioPlanSelect.forEach((plan, index) => {
-    plan.addEventListener('click', (e)=> {
-        openPlan(index)
-    })
-})
-
-headlinePlanSelect.forEach((plan, index) => {
-    plan.addEventListener('click', (e)=> {
-        openPlan(index)
-    })
-})
-
 pledgeButton.forEach((button, index)=>{
     button.addEventListener('click', (e)=> {
         let minValue = parseInt(inputFields[index].getAttribute('min'));
@@ -96,30 +135,31 @@ pledgeButton.forEach((button, index)=>{
         const parentElement = document.querySelectorAll('.modal-donation-card-pledge');
         if (currentValue < minValue || isNaN(currentValue)){
             parentElement[index].classList.add('modal-donation-card-pledge-error');
+            const errorMsg = document.querySelectorAll('.error-msg');
+            errorMsg[index].innerText = `Minimum value for this plan is ${minValue}$`
         }else {
             parentElement[index].classList.remove('modal-donation-card-pledge-error');
             parentElement[index].classList.add('modal-donation-card-pledge-loader');
             inputFields[index].value = '';
             trackerUpdate(currentValue)
             setTimeout(() => {
-                parentElement[index].classList.remove('modal-donation-card-pledge-loader') 
+                parentElement[index].classList.remove('modal-donation-card-pledge-loader');
+                modalContent.style.display = "none";
+                thanksModal.style.display = 'block';
             }, 1000);
         }
     })
 })
 
 const trackerUpdate =(value)=> {
-    const currentMoneyDisplay = document.getElementById('current-sum');
-    const currentBackersDisplay = document.getElementById('backers');
-    const trackerLine = document.getElementById('inner-line');
     const maxMoney = 100000;
     currentMoney += value
     currentBackers ++;
-    currentMoneyDisplay.innerText = currentMoney.toLocaleString('en-US')
+    currentMoneyDisplay.innerText = `$${currentMoney.toLocaleString('en-US')}`;
     currentBackersDisplay.innerText = currentBackers.toLocaleString('en-US');
     if (currentMoney >= maxMoney){
         trackerLine.style.width = '100%'
     }else {
-        trackerLine.style.width = `${parseInt(currentMoneyDisplay.innerText)}%`
+        trackerLine.style.width = `${parseInt(currentMoneyDisplay.innerText.split('$')[1].split(',')[0])}%`
     }
 }
